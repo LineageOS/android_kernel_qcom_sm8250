356a5affcb4ab9f09444f2a91200022e0b6ce680
Kees Cook <keescook@chromium.org>
2018-10-10 17:18:24 -0700

    UPSTREAM: LSM: Record LSM name in struct lsm_info
    In preparation for making LSM selections outside of the LSMs, include
    the name of LSMs in struct lsm_info.
    
    Change-Id: I129da3575cd955862cd00e139ee04d94a79bd1b4
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: James Morris <james.morris@microsoft.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -1483,6 +1483,7 @@ extern void security_add_hooks(struct security_hook_list *hooks, int count,
 				char *lsm);
 
 struct lsm_info {
+	const char *name;	/* Required. */
 	int (*init)(void);	/* Required. */
 };
 
