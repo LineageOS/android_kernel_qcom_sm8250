2f0038e4d42a76c6e387367f5e8c786246afd4a3
David Ahern <dsahern@gmail.com>
2019-03-21 05:21:34 -0700

    UPSTREAM: ipv6: Move setting default metric for routes
    ip6_route_info_create is a low level function for ensuring fc_metric is
    set. Move the check and default setting to the 2 locations that do not
    already set fc_metric before calling ip6_route_info_create. This is
    required for the next patch which moves addrconf allocations to
    ip6_route_info_create and want the metric for host routes to be 0.
    
    Change-Id: I30e20612da2ee923b6db844d72bb23fa974f8b32
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -3066,9 +3066,6 @@ static struct fib6_info *ip6_route_info_create(struct fib6_config *cfg,
 			goto out;
 	}
 
-	if (cfg->fc_metric == 0)
-		cfg->fc_metric = IP6_RT_PRIO_USER;
-
 	if (cfg->fc_flags & RTNH_F_ONLINK) {
 		if (!dev) {
 			NL_SET_ERR_MSG(extack,
@@ -3689,7 +3686,7 @@ static void rtmsg_to_fib6_config(struct net *net,
 		.fc_table = l3mdev_fib_table_by_index(net, rtmsg->rtmsg_ifindex) ?
 			 : RT6_TABLE_MAIN,
 		.fc_ifindex = rtmsg->rtmsg_ifindex,
-		.fc_metric = rtmsg->rtmsg_metric,
+		.fc_metric = rtmsg->rtmsg_metric ? : IP6_RT_PRIO_USER,
 		.fc_expires = rtmsg->rtmsg_info,
 		.fc_dst_len = rtmsg->rtmsg_dst_len,
 		.fc_src_len = rtmsg->rtmsg_src_len,
@@ -4634,6 +4631,9 @@ static int inet6_rtm_newroute(struct sk_buff *skb, struct nlmsghdr *nlh,
 	err = rtm_to_fib6_config(skb, nlh, &cfg, extack);
 	if (err < 0)
 		return err;
+
+	if (cfg.fc_metric == 0)
+		cfg.fc_metric = IP6_RT_PRIO_USER;
 
 	if (cfg.fc_mp)
 		return ip6_route_multipath_add(&cfg, extack);
