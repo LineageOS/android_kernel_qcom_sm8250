e55e807c1f0621a01386e385c28446241a8067ea
YueHaibing <yuehaibing@huawei.com>
2019-04-29 13:56:11 +0000

    UPSTREAM: bpf: Use PTR_ERR_OR_ZERO in bpf_fd_sk_storage_update_elem()
    Use PTR_ERR_OR_ZERO rather than if(IS_ERR(...)) + PTR_ERR
    
    Change-Id: Iee1e717111237b500f96646497fe8e73f034ad8f
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- net/core/bpf_sk_storage.c
+++ net/core/bpf_sk_storage.c
@@ -140,7 +140,7 @@ static int bpf_fd_sk_storage_update_elem(struct bpf_map *map, void *key,
 			sock->sk, (struct bpf_local_storage_map *)map, value,
 			map_flags);
 		sockfd_put(sock);
-		return IS_ERR(sdata) ? PTR_ERR(sdata) : 0;
+		return PTR_ERR_OR_ZERO(sdata);
 	}
 
 	return err;
