a04dd5492b2d3220d0096ac9f263f6426c960d72
Daniel Rosenberg <drosen@google.com>
2025-02-11 13:36:52 -0800

    UPSTREAM: ANDROID: fuse-bpf: Correct fuse bpf feature flag
    The feature flag should only advertise fuse-bpf if fuse-bpf is a
    supported feature
    
    Bug: 372951405
    Test: Compile with CONFIG_FUSE_BPF unset
    Change-Id: I0049a3075f78576499168b8ebb6e833ccd18db0f
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1887,6 +1887,7 @@ static void fuse_fs_cleanup(void)
 
 static struct kobject *fuse_kobj;
 
+#ifdef CONFIG_FUSE_BPF
 static ssize_t fuse_bpf_show(struct kobject *kobj,
 				       struct kobj_attribute *attr, char *buff)
 {
@@ -1895,9 +1896,12 @@ static ssize_t fuse_bpf_show(struct kobject *kobj,
 
 static struct kobj_attribute fuse_bpf_attr =
 		__ATTR_RO(fuse_bpf);
+#endif
 
 static struct attribute *bpf_features[] = {
+#ifdef CONFIG_FUSE_BPF
 	&fuse_bpf_attr.attr,
+#endif
 	NULL,
 };
 
