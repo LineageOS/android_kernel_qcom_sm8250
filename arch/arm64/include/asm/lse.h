24953a7f0ce8f62ea9308adc0c738465b70c6182
Atakan <ata.kaner03@gmail.com>
2025-08-21 11:08:58 +0300

    Revert "arm64: lse: fix LSE atomics with LLVM's integrated assembler"
    It will be repicked from upstream to reduce future conflicts.
    
    This reverts commit 5681cce44cf41dc3eed8fd62b9e299bb57d1cb7b.
    
    Change-Id: If9dfbe81897f52359b9c73ef7bd250d8f4114601

--- arch/arm64/include/asm/lse.h
+++ arch/arm64/include/asm/lse.h
@@ -4,8 +4,6 @@
 
 #if defined(CONFIG_AS_LSE) && defined(CONFIG_ARM64_LSE_ATOMICS)
 
-#define __LSE_PREAMBLE	".arch armv8-a+lse\n"
-
 #include <linux/compiler_types.h>
 #include <linux/export.h>
 #include <linux/stringify.h>
