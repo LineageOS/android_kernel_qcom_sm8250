07429e7ed7333cf6d55712cb727d0a7faf60bf53
Gaurav Singh <gauravsi@qti.qualcomm.com>
2025-09-30 08:28:46 -0700

    kgsl: Add buffer overflow check for perfcounter dynamic list
    Add buffer overflow check to ensure dynamic list updates do not
    exceed allocated buffer size, returning an error if overflow
    would occur.
    
    Change-Id: If9a1817a87e7a8f74dbffe369abd27a1519dc96b
    Signed-off-by: Shiv Kumar <shikum@qti.qualcomm.com>
    Signed-off-by: Gaurav Singh <gauravsi@qti.qualcomm.com>

--- drivers/gpu/msm/adreno_a6xx.c
+++ drivers/gpu/msm/adreno_a6xx.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/firmware.h>
@@ -2659,6 +2660,7 @@ static int a6xx_perfcounter_update(struct adreno_device *adreno_dev,
 	struct cpu_gpu_lock *lock = ptr;
 	u32 *data = ptr + sizeof(*lock);
 	int i, offset = 0;
+	u32 pending_pairs = 2; /* No of pairs to add: <select,value> and <cntl,1> */
 
 	if (cpu_gpu_lock(lock)) {
 		cpu_gpu_unlock(lock);
@@ -2678,6 +2680,11 @@ static int a6xx_perfcounter_update(struct adreno_device *adreno_dev,
 
 		offset += 2;
 	}
+
+	/* Ensure there is enough space in the reglist buffer for new pairs */
+	if ((offset + (pending_pairs * 2)) >=
+		(adreno_dev->pwrup_reglist->size / sizeof(u32)))
+		return -ENOSPC;
 
 	/*
 	 * For a612 targets A6XX_RBBM_PERFCTR_CNTL needs to be the last entry,
