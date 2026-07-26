0a8c73b074c95b523222abcf4384913616177972
Oleg Nesterov <oleg@redhat.com>
2026-07-26 09:35:19 -0400

    taskstats: fill_stats_for_tgid: use for_each_thread()
    [ Upstream commit ed5378a387fd7c382497f2abcf4605e030b64044 ]
    
    do/while_each_thread should be avoided when possible.
    
    Plus I _think_ this change allows to avoid lock_task_sighand() but I am
    not sure, I forgot everything about taskstats.  In any case, this code
    does not look right in that the same thread can be accounted twice:
    taskstats_exit() can account the exiting thread in signal->stats and drop
    ->siglock but this thread is still on the thread-group list, so
    lock_task_sighand() can't help.
    
    Link: https://lkml.kernel.org/r/20230909214951.GA24274@redhat.com
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Cc: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Stable-dep-of: b3e4fbb04220 ("taskstats: retain dead thread stats in TGID queries")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- kernel/taskstats.c
+++ kernel/taskstats.c
@@ -225,9 +225,8 @@ static int fill_stats_for_tgid(pid_t tgid, struct taskstats *stats)
 	else
 		memset(stats, 0, sizeof(*stats));
 
-	tsk = first;
 	start_time = ktime_get_ns();
-	do {
+	for_each_thread(first, tsk) {
 		if (tsk->exit_state)
 			continue;
 		/*
@@ -250,7 +249,7 @@ static int fill_stats_for_tgid(pid_t tgid, struct taskstats *stats)
 
 		stats->nvcsw += tsk->nvcsw;
 		stats->nivcsw += tsk->nivcsw;
-	} while_each_thread(first, tsk);
+	}
 
 	unlock_task_sighand(first, &flags);
 	rc = 0;
